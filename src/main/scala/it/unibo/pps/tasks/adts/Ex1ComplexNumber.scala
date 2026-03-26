9f0ec992858e2d992059b6eff0cd165c5f8090e7
Elisa Yan <elisa.yan@studio.unibo.it>
2026-03-26 10:46:54 +0100

    implemented task 1

--- src/main/scala/it/unibo/pps/tasks/adts/Ex1ComplexNumber.scala
+++ src/main/scala/it/unibo/pps/tasks/adts/Ex1ComplexNumber.scala
@@ -1,5 +1,7 @@
 package it.unibo.pps.tasks.adts
 
+import it.unibo.pps.u04.adts.SequenceADT.*
+
 /*  Exercise 1: 
  *  Complete the implementation of ComplexADT trait below, so that it passes
  *  the test in ComplexTest.
@@ -9,7 +11,9 @@ object Ex1ComplexNumbers:
 
   trait ComplexADT:
     type Complex
+
     def complex(re: Double, im: Double): Complex
+
     extension (complex: Complex)
       def re(): Double
       def im(): Double
@@ -19,12 +23,27 @@ object Ex1ComplexNumbers:
 
   object BasicComplexADT extends ComplexADT:
 
+    import it.unibo.pps.u04.adts.SequenceADT.*
+
+    case class ComplexImpl(reVal: Double, imVal: Double)
+
     // Change assignment below: should probably define a case class and use it?
-    type Complex = Nothing 
-    def complex(re: Double, im: Double): Complex = ???
+    opaque type Complex = ComplexImpl
+
+    def complex(re: Double, im: Double): Complex =
+      ComplexImpl(re, im)
+
     extension (complex: Complex)
-      def re(): Double = ???
-      def im(): Double = ???
-      def sum(other: Complex): Complex = ???
-      def subtract(other: Complex): Complex = ???
-      def asString(): String = ???
+      def re(): Double = complex.reVal
+      def im(): Double = complex.imVal
+      def sum(other: Complex): Complex = ComplexImpl(complex.reVal + other.reVal, complex.imVal + other.imVal)
+      def subtract(other: Complex): Complex = ComplexImpl(complex.reVal - other.reVal, complex.imVal - other.imVal)
+      def asString(): String =
+        val r = complex.reVal
+        val i = complex.imVal
+        (r, i) match
+          case (re, 0) => s"$re"
+          case (0, im) => s"${im}i"
+          case (re, im) =>
+            val sign = if im > 0 then "+" else "-"
+            s"$re $sign ${math.abs(im)}i"
